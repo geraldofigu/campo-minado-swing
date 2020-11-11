f994e4a6e898c65183cd2f9eb74678a417256b24
geraldofigu <74158687+geraldofigu@users.noreply.github.com>
2020-11-11 09:44:15 -0300

    Initial commit

---
+++ src/cm/modelo/CampoEvento.java
@@ -0,0 +1,7 @@
+package cm.modelo;
+
+public enum CampoEvento {
+	
+	ABRIR, MARCAR, DESMARCAR, EXPLODRIR, REINICIAR;
+
+}
---
+++ src/cm/visao/TelaPrincipal.java
@@ -0,0 +1,31 @@
+package cm.visao;
+
+import javax.swing.JFrame;
+
+import cm.modelo.Tabuleiro;
+
+public class TelaPrincipal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	public TelaPrincipal() {
+		Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+		add(new PainelTabuleiro(tabuleiro));
+		
+		setTitle("Campo Minado");
+		setSize(690, 438);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+		new TelaPrincipal();
+		
+		
+		
+	}
+	
+
+}
